4c1a8736e33b65d889a48e0708c2329f18c8bb7f
Kang Cahya <cahyadyazin@yahoo.com>
2024-03-10 18:59:51 +0700

    Update sql_helper.js
    update

--- sql_helper.js
+++ sql_helper.js
@@ -7,76 +7,116 @@ import { openOrCreate } from "@nativescript-community/sqlite";
 */
 
 const sqlite = openOrCreate("dbname.db");
+const env = "production"; // development, production
 
 export async function SQL__select(table, fields = "*", conditionalQuery) {
-  const select = "SELECT " + fields + " FROM " + table + " " + conditionalQuery;
-  const data = await sqlite.select(select);
-  return data;
+  try {
+    const select =
+      "SELECT " + fields + " FROM " + table + " " + conditionalQuery;
+    const data = await sqlite.select(select);
+    return data;
+  } catch (error) {
+    if (env === "development") {
+      console.log("SQL__select error >> ", error);
+    }
+  }
 }
 
 export async function SQL__insert(table, data = []) {
-  if (data.length) {
-    let fields = [],
-      holder = [],
-      value = [];
-    for (let i in data) {
-      fields.push(data[i].field);
-      holder.push("?");
-      value.push(data[i].value);
+  try {
+    if (data.length) {
+      let fields = [],
+        holder = [],
+        value = [];
+      for (let i in data) {
+        fields.push(data[i].field);
+        holder.push("?");
+        value.push(data[i].value);
+      }
+
+      let fieldsString = fields.join(", "),
+        holderString = holder.join(", ");
+
+      const insert =
+        "INSERT INTO " +
+        table +
+        " (" +
+        fieldsString +
+        ") VALUES (" +
+        holderString +
+        ")";
+
+      await sqlite.execute(insert, value);
+    } else {
+      console.dir("Data : ", data);
+      console.dir("Data length : ", data.length);
+      console.log("No data to insert");
     }
-
-    let fieldsString = fields.join(", "),
-      holderString = holder.join(", ");
-
-    const insert =
-      "INSERT INTO " +
-      table +
-      " (" +
-      fieldsString +
-      ") VALUES (" +
-      holderString +
-      ")";
-    await sqlite.execute(insert, value);
-  } else {
-    console.dir("Data : ", data);
-    console.dir("Data length : ", data.length);
-    console.log("No data to insert");
+  } catch (error) {
+    if (env === "development") {
+      console.log("SQL__insert error >> ", error);
+    }
   }
 }
 
 export async function SQL__update(table, data = [], id, conditionalQuery) {
-  if (data.length) {
-    let dataSet = [];
-    for (let i in data) {
-      dataSet.push(data[i].field + " = " + data[i].value);
-    }
+  try {
+    if (data.length) {
+      let dataSet = [];
+      for (let i in data) {
+        dataSet.push(data[i].field + " = " + data[i].value);
+      }
 
-    let dataSetString = dataSet.join(", ");
+      let dataSetString = dataSet.join(", ");
 
-    if (id) {
-      const update =
-        "UPDATE " + table + " SET " + dataSetString + " WHERE id=" + id;
+      if (id) {
+        const update =
+          "UPDATE " + table + " SET " + dataSetString + " WHERE id=" + id;
+      } else {
+        const update =
+          "UPDATE " + table + " SET " + dataSetString + " " + conditionalQuery;
+      }
+      await sqlite.execute(update);
     } else {
-      const update =
-        "UPDATE " + table + " SET " + dataSetString + " " + conditionalQuery;
+      console.dir("Data : ", data);
+      console.dir("Data length : ", data.length);
+      console.log("No data to update");
     }
-    await sqlite.execute(update);
-  } else {
-    console.dir("Data : ", data);
-    console.dir("Data length : ", data.length);
-    console.log("No data to update");
+  } catch (error) {
+    if (env === "development") {
+      console.log("SQL__update error >> ", error);
+    }
   }
 }
 
 export async function SQL__delete(table, conditionalQuery) {
-  await sqlite.execute("DELETE FROM " + table + " " + conditionalQuery);
+  try {
+    await sqlite.execute("DELETE FROM " + table + " " + conditionalQuery);
+  } catch (error) {
+    if (env === "development") {
+      console.log("SQL__delete error >> ", error);
+    }
+  }
 }
 
 export async function SQL__truncate(table) {
-  await sqlite.execute("DELETE FROM " + table);
-  await sqlite.execute("VACUUM");
+  try {
+    await sqlite.execute("DELETE FROM " + table);
+    await sqlite.execute("VACUUM");
+  } catch (error) {
+    if (env === "development") {
+      console.log("SQL__truncate error >> ", error);
+    }
+  }
 }
 
 export async function SQL__query(query) {
-  await sqlite.execute(query);
+  try {
+    const data = await sqlite.execute(query);
+    return data;
+  } catch (error) {
+    if (env === "development") {
+      console.log("SQL__query error >> ", error);
+    }
+  }
 }
